871e2dece4e5177dfb36a5231bfe0684d1913778
ranjith <ranjithkankati2@gmail.com>
2026-05-21 10:52:28 +0530

    string interpolation for vercel check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-Newprofile';
+  title = 'ProfileDetails';
+  name = 'Ranjith';
+  age = 26;
+  role = 'Frontend Engineer';
+  skills = 'angular/react'
 }
